d1bdf289355fda2e7a838f491d74da48b4fd71ff
Aakash070802 <aakash.kash92@gmail.com>
2026-07-02 00:36:15 +0530

    feat: update dependencies and enhance project configuration with path aliases

--- apps/frontend/src/app/router/AppRouter.tsx
+++ apps/frontend/src/app/router/AppRouter.tsx
@@ -3,7 +3,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 function HomePage() {
   return (
     <main className="flex min-h-screen items-center justify-center bg-zinc-950">
-      <h1 className="text-5xl font-bold text-white">LinkSphere 🚀</h1>
+      <h1 className="text-5xl font-bold text-white">LinkSphere 🚀 HEllo</h1>
     </main>
   );
 }
