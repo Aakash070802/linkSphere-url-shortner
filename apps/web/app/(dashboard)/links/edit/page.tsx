d01f83462aa6df05d1fff56aabd4be51d206e300
Aakash070802 <aakash.kash92@gmail.com>
2026-07-16 20:19:53 +0530

    feat: add initial pages and layouts for authentication, dashboard, and marketing sections

---
+++ apps/web/app/(dashboard)/links/edit/page.tsx
@@ -0,0 +1,3 @@
+export default function EditLinkPage() {
+  return <div>Edit Link</div>;
+}
---
+++ apps/web/app/error.tsx
@@ -0,0 +1,5 @@
+'use client';
+
+export default function Error({ error, reset }: { error: Error; reset: () => void }) {
+  return <div>Something went wrong</div>;
+}
